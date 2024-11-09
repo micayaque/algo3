39ecc283cd034eb5e2ef04989fa0fb14f31ea7fa
Micaela <myaque@dc.uba.ar>
2024-11-09 17:08:47 -0300

    add implementacion prim

---
+++ p3/prim.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+	int spanningTree(int V, vector<vector<int>> adj[])
+	{
+		priority_queue<pair<int, int>,
+		               vector<pair<int, int> >, greater<pair<int, int>>> pq;
+
+		vector<int> vis(V, 0);
+		// {wt, node}
+		pq.push({0, 0});
+		int sum = 0;
+		while (!pq.empty()) {
+			auto it = pq.top();
+			pq.pop();
+			int node = it.second;
+			int weight = it.first;
+
+			if (vis[node] == 1) continue;
+            
+			// Lo agrego al AGM
+			vis[node] = 1;
+			sum += weight;
+			for (auto it : adj[node]) {
+				int adjNode = it[0];
+				int edW = it[1];
+				if (!vis[adjNode]) {
+					pq.push({edW, adjNode});
+				}
+			}
+		}
+		return sum;
+	}
+};
